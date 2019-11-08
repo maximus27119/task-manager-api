020f36839c341687ba394af454e3347a2c31efdf
Maxim Usyk <max.taina32@gmail.com>
2019-11-08 21:31:30 +0200

    Filtering, pagination, sorting functions was added

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -3,11 +3,36 @@ const Task = require('../models/task');
 const auth = require('../middleware/auth');
 const router = new express.Router();
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:desc
+
 router.get('/tasks', auth, async (req, res) => { // Получить массив тасков
+    const match = {};
+    const sort = {};
+
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true';
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+
     try{
+
         // const tasks = await Task.find({ owner: req.user._id });
         
-        await req.user.populate('tasks').execPopulate();
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate();
         
         res.send(req.user.tasks);
         
