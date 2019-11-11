cf2f40cb19abba1acfc2986a435ef7a8aec668e7
Maxim Usyk <35966690+maximus27119@users.noreply.github.com>
2019-11-11 23:23:04 +0200

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,23 +8,3 @@ mongoose.connect(mongoDbUrl, {
     useCreateIndex: true,
     useFindAndModify: false
 });
-
-// const myTask = new Task({
-//     description: '       Eat lunch        ',
-// }).save().then((task) => {
-//     console.log(task);
-// }).catch((error) => {
-//     console.log(error);
-// });
-
-// const me = new User({
-//     name: '    Bohdan   ',
-//     email: 'Bohdan@GMAIL.COM     ',
-//     password: '  1234567'
-// });
-
-// me.save().then((me) => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log(error);
-// });
